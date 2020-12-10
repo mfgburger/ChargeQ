e1ce7efc0a3951d14f9a5d4538da5c31e41975fb
phkrueger <ph.krueger@tum.de>
2020-12-10 15:23:59 +0100

    added state of charge

--- Backend/optimizer.py
+++ Backend/optimizer.py
@@ -4,12 +4,14 @@ import numpy as np
 
 def random_car_list(length=5):
     ranges = np.random.poisson(lam=10, size=length)  # in km
+    charges = np.random.uniform(low=0.0, high=1.0, size=length) # charge from 0-1
     times = np.random.poisson(lam=10, size=length)  # in h
     priorities = np.random.randint(2, size=length) + 1  # 1: eco, 2: fast
     consumptions = [0.2]*length #kW/km #todo https://www.virta.global/blog/ev-charging-101-how-much-electricity-does-an-electric-car-use
     speeds = 10 ** (np.random.randint(2,
                                       size=length) + 1)  # 10kW or 100kW https://www.transportenvironment.org/sites/te/files/publications/01%202020%20Draft%20TE%20Infrastructure%20Report%20Final.pdf
     list_of_cars = [Car(id=uuid.uuid4().hex,
+                        state_of_charge = charges[i],
                         target_range=ranges[i],
                         hours_to_departure=times[i],
                         consumption_per_km=consumptions[i],
@@ -20,29 +22,31 @@ def random_car_list(length=5):
     return list_of_cars
 
 
-class Optimizer:
-
-    def __init__(self):
-        some = 0
-
-    def calculate(self):
-        pass
-
-    def calculate_quantum(self):
-        pass
+def optimize(list_of_cars):
+    loading_actions = []
+    return loading_actions
 
 
-def estimate_need(target_range, hours_to_departure, consumption_per_km):
-    hours_to_departure = hours_to_departure
-    delta_capacity = target_range * consumption_per_km
-    return delta_capacity, hours_to_departure
+class LoadingAction:
+
+    def __init__(self, car, power, start_time, end_time):
+        self._car = car
+        self._power = power
+        self._start_time = start_time
+        self._end_time = end_time
+
+    def calculate_cost(self):
+        return self._car.estimate_delta_capacity/self.car
 
 
 class Car:
 
-    def __init__(self, id, target_range, hours_to_departure, consumption_per_km, is_priority, max_charging_speed, station_id):
+    def __init__(self, id, state_of_charge, target_range, hours_to_departure, consumption_per_km, is_priority, max_charging_speed, station_id):
         self._id = id
-        self._est_delta_capacity, self._est_hours_to_departure = estimate_need(target_range, hours_to_departure, consumption_per_km)
+        self._state_of_charge = state_of_charge
+        self._target_range = target_range
+        self._hours_to_departure = hours_to_departure
+        self._consumption_kw_per_km = consumption_per_km
         self._is_priority = is_priority
         self._max_charging_speed = max_charging_speed
         self._station_id = station_id
@@ -50,7 +54,12 @@ class Car:
     def print_values(self):
         print(self.__dict__)
 
+    def estimate_delta_capacity(self):
+        delta_capacity = self._target_range * self._consumption_kw_per_km # todo: naive
+        return delta_capacity
+
 
 if __name__ == "__main__":
-    car_list = random_car_list(5)
-    [car.print_values() for car in car_list]
+    car_list = random_car_list(5) # generate a number of cars for fun
+    [car.print_values() for car in car_list] # Look at the generated values for fun
+    optimize(car_list) # give him the list for optimization
