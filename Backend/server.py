8f6c2b670a6c1049f810bdec87b624bca94ad671
Florian Burger <florian.burger@mpq.mpg.de>
2020-12-13 18:40:07 +0100

    API testing.

--- Backend/server.py
+++ Backend/server.py
@@ -5,8 +5,10 @@
 #############################################
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def greet_user():
@@ -28,5 +30,7 @@ def return_dummy():
 def optimize():
     if request.method == "GET":
         # return the current queue
+        pass
     else:
         # Pass POST data to optimizer and return new queue
+        pass
